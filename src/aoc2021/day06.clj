0672ac8e886cafba0e6db4bf0fa1795f07c78746
Alexandar Guzsvany <git@guzsvany.de>
2021-12-20 19:46:22 +0100

    Day 6 part 2

--- src/aoc2021/day06.clj
+++ src/aoc2021/day06.clj
@@ -2,6 +2,11 @@
   (:require [clojure.string :as str]
             [aoc2021.util :as u]))
 
+;;;
+;;; naive / slow
+;;; by keeping the input sequence and literally growing the string
+;;;
+
 (defn parse-input
   "Input: (\"n,n,...\") ; list with one string with numbers separated by comma
   Output: (n n ...) ; list of integers"
@@ -11,7 +16,7 @@
     (clojure.string/split $ #"[, ]+")
     (map #(Integer/parseInt %) $)))
 
-(defn calc-next-day
+(defn calc-next-day-naive
   "Input: (n[d] m[d] ...)
   Output: (n[d+1] m[d+1] ... x[d+1])"
   [fishes]
@@ -19,7 +24,7 @@
         fishes-next-day (map #(if (= 0 %) 6 (dec %)) fishes)]
     (concat fishes-next-day (repeat zero-count 8)))) ; 6 or 8
 
-(defn loop-days
+(defn loop-days-naive
   [initial-fishes last-day]
   (loop [day 0
          fishes initial-fishes]
@@ -29,17 +34,66 @@
       [day (count fishes)]
       (do
         (recur (inc day)
-               (calc-next-day fishes))))))
+               (calc-next-day-naive fishes))))))
+
+;;;
+;;; based on counters
+;;; by keeping track of how many fishes of a specific age exists
+;;;
+
+;          0 1 2 3 4 5 6 7 8
+; fishes:  0 0 0 0 0 0 1 0 0 "6"
+; day 1:   0 0 0 0 0 1 0 0 0 "5"
+;     5:   0 1 0 0 0 0 0 0 0 "1"
+;     6:   1 0 0 0 0 0 0 0 0 "0"
+;     7:   0 0 0 0 0 0 1 0 1 "6,8"
+;     8:   0 0 0 0 0 1 0 1 0 "5,7"
+;     9:   0 0 0 0 1 0 1 0 0 "4,6"
+(defn calc-next-day-vec
+  [fishes]
+  (let [z (first fishes)]
+    (-> fishes
+        (subvec 1)
+        (update 6 + z)
+        (conj z))))
+
+(defn loop-days-vec
+  [initial-fishes last-day]
+  (loop [day 0
+         fishes (reduce (fn [f n] (update f n inc))
+                        (vec (repeat 9 0))
+                        initial-fishes)]
+    (if (= day last-day)
+      [day fishes (apply + fishes)]
+      (do
+        (println day fishes (apply + fishes))
+        (recur (inc day)
+               (calc-next-day-vec fishes))))))
+
+;;;
+;;; part 1
+;;;
 
 (defn solve-part-1
   "80 days of lanternfish"
   [lines]
-  (loop-days (parse-input lines)
-             (let [n (second lines)]
-               (if n (Integer/parseInt n) 80))))
+  ;(loop-days-naive
+  (loop-days-vec
+    (parse-input lines)
+    ; second line can be the number of days
+    (let [n (second lines)]
+      (if n (Integer/parseInt n) 80))))
 
+;;;
+;;; part 2
+;;;
 
 (defn solve-part-2
   "256 days of lanternfish"
   [lines]
-  "algo of part 1 does not terminate with days=256...")
+  (loop-days-vec
+    (parse-input lines)
+    ; second line can be the number of days
+    (let [n (second lines)]
+      (if n (Integer/parseInt n) 256))))
+
